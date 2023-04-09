a8c9f52a70b0c9d011c99ffec5fc82a1b593bba6
MrKamlakar <113072762+MrKamlakar@users.noreply.github.com>
2023-04-09 16:17:32 +0530

    Update run.py

--- run.py
+++ run.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template("home.html")
+    return render_template("index.html")
 
 @app.route('/predict',methods=['POST','GET'])
 def result():
@@ -42,4 +42,4 @@ def result():
 
 
 if __name__ == "__main__":
-    app.run(debug=True, port=9457)
+    app.run(debug=True)
