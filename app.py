da7ae4041f5c9ce3f2f7d8c36fc8a9194242f28d
MrKamlakar <113072762+MrKamlakar@users.noreply.github.com>
2023-04-09 14:37:33 +0530

    Update app.py

--- app.py
+++ app.py
@@ -26,13 +26,13 @@ def result():
     X= np.array([[ item_weight,item_fat_content,item_visibility,item_type,item_mrp,
                   outlet_establishment_year,outlet_size,outlet_location_type,outlet_type ]])
 
-    scaler_path= r'D:\5th_semester\MiniProject2A\Projectworking\models\sc.sav'
+    scaler_path= r'D:\final\Project2A\Projectworking\models\sc.sav'
 
     sc=joblib.load(scaler_path)
 
     X_std= sc.transform(X)
 
-    model_path=r'D:\5th_semester\MiniProject2A\Projectworking\models\lr.sav'
+    model_path=r'D:\final\Project2A\Projectworking\models\lr.sav'
 
     model= joblib.load(model_path)
 
